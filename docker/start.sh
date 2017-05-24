15c08248ae218605d9210fd055474d4d64330ab5
lbrtcoin@gmail.com <lbrtcoin@gmail.com>
2017-05-24 17:37:10 +0000

    conf path change

--- docker/start.sh
+++ docker/start.sh
@@ -4,5 +4,5 @@ if [ "$(docker ps -a | grep [w]allet)" != "" ];
 then
   docker start wallet
 else
-  docker run -d --name wallet -p 9332:9332 -p 9333:9333 -v $(pwd)/conf:/root/albertcoin/conf -v $(pwd)/data:/root/albertcoin/data wallet 
+  docker run -d --name wallet -p 9332:9332 -p 9333:9333 -v $(pwd)/conf:/root/.albertcoin -v $(pwd)/data:/root/albertcoin/data wallet 
 fi
